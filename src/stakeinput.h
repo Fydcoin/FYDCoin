ef19e838f594db3fe6e438e0e449ba465c159b0d
Fydcoin <48912706+Fydcoin@users.noreply.github.com>
2019-07-01 20:45:14 +0800

    v1.1.0

---
+++ src/stakeinput.h
@@ -0,0 +1,90 @@
+// Copyright (c) 2017-2018 FYD
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef FYD_STAKEINPUT_H
+#define FYD_STAKEINPUT_H
+
+class CKeyStore;
+class CWallet;
+class CWalletTx;
+
+class CStakeInput
+{
+protected:
+    CBlockIndex* pindexFrom;
+
+public:
+    virtual ~CStakeInput(){};
+    virtual CBlockIndex* GetIndexFrom() = 0;
+    virtual bool CreateTxIn(CWallet* pwallet, CTxIn& txIn, uint256 hashTxOut = 0) = 0;
+    virtual bool GetTxFrom(CTransaction& tx) = 0;
+    virtual CAmount GetValue() = 0;
+    virtual bool CreateTxOuts(CWallet* pwallet, vector<CTxOut>& vout, CAmount nTotal) = 0;
+    virtual bool GetModifier(uint64_t& nStakeModifier) = 0;
+    virtual bool IsZFYD() = 0;
+    virtual CDataStream GetUniqueness() = 0;
+};
+
+
+// zFYDStake can take two forms
+// 1) the stake candidate, which is a zcmint that is attempted to be staked
+// 2) a staked zfyd, which is a zcspend that has successfully staked
+class CZFydStake : public CStakeInput
+{
+private:
+    uint32_t nChecksum;
+    bool fMint;
+    libzerocoin::CoinDenomination denom;
+    uint256 hashSerial;
+
+public:
+    explicit CZFydStake(libzerocoin::CoinDenomination denom, const uint256& hashSerial)
+    {
+        this->denom = denom;
+        this->hashSerial = hashSerial;
+        this->pindexFrom = nullptr;
+        fMint = true;
+    }
+
+    explicit CZFydStake(const libzerocoin::CoinSpend& spend);
+
+    CBlockIndex* GetIndexFrom() override;
+    bool GetTxFrom(CTransaction& tx) override;
+    CAmount GetValue() override;
+    bool GetModifier(uint64_t& nStakeModifier) override;
+    CDataStream GetUniqueness() override;
+    bool CreateTxIn(CWallet* pwallet, CTxIn& txIn, uint256 hashTxOut = 0) override;
+    bool CreateTxOuts(CWallet* pwallet, vector<CTxOut>& vout, CAmount nTotal) override;
+    bool MarkSpent(CWallet* pwallet, const uint256& txid);
+    bool IsZFYD() override { return true; }
+    int GetChecksumHeightFromMint();
+    int GetChecksumHeightFromSpend();
+    uint32_t GetChecksum();
+};
+
+class CFydStake : public CStakeInput
+{
+private:
+    CTransaction txFrom;
+    unsigned int nPosition;
+public:
+    CFydStake()
+    {
+        this->pindexFrom = nullptr;
+    }
+
+    bool SetInput(CTransaction txPrev, unsigned int n);
+
+    CBlockIndex* GetIndexFrom() override;
+    bool GetTxFrom(CTransaction& tx) override;
+    CAmount GetValue() override;
+    bool GetModifier(uint64_t& nStakeModifier) override;
+    CDataStream GetUniqueness() override;
+    bool CreateTxIn(CWallet* pwallet, CTxIn& txIn, uint256 hashTxOut = 0) override;
+    bool CreateTxOuts(CWallet* pwallet, vector<CTxOut>& vout, CAmount nTotal) override;
+    bool IsZFYD() override { return false; }
+};
+
+
+#endif //FYD_STAKEINPUT_H
